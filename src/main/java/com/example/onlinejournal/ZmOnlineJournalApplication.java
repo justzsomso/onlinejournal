3f37509f0a3b2a7bb1aed952bb54a767738d9300
Zhou Min <132646373+justzsomso@users.noreply.github.com>
2025-01-07 21:16:20 +0800

    feat(修改配置类): 修改文件名，项目名称，新增redis相关依赖，新增mysql相关依赖，新增sa-token相关依赖

--- src/main/java/com/example/onlinejournal/ZmOnlineJournalApplication.java
+++ src/main/java/com/example/onlinejournal/ZmOnlineJournalApplication.java
@@ -1,4 +1,4 @@
-package com.example.zmOnlinejournal;
+package com.example.onlinejournal;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
